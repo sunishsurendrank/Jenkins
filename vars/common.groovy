c5cfa8f4c53bbeb330b802a13d625fd706e220af
root <root@lab1.novalocal>
2020-08-20 17:07:08 +0300

    added the library files

---
+++ vars/common.groovy
@@ -0,0 +1,12 @@
+def dockerPushImage
+{
+
+withCredentials([
+            usernamePassword(credentialsId: 'dockerhubCredential',
+              usernameVariable: 'username',
+              passwordVariable: 'password')
+          ])
+
+          sh "docker login -u username -p password;docker push  sunishsurendrank/webserver:v$BUILD_NUMBER"
+
+}
