d754f808d65cbf247b16bb8bbdbeface8956773e
Sunish Surendran K <sunishsurendrank@gmail.com>
2020-08-20 20:06:50 +0530

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,10 +1,13 @@
 def dockerPushImage
 {
+ 
+ steps{
 
-withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'dockerhubCredential',
-usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'dockerhubCredential',
+   usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
 
-sh 'echo uname=$USERNAME pwd=$PASSWORD'
+   sh 'echo uname=$USERNAME pwd=$PASSWORD'
+    }
  }
 
 
