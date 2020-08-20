d661c234f56d2acd037b189b10b9b41a652cd7dc
Sunish Surendran K <sunishsurendrank@gmail.com>
2020-08-20 19:54:35 +0530

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -5,11 +5,12 @@ withCredentials([
             usernamePassword(credentialsId: 'dockerhubCredential',
               usernameVariable: 'username',
               passwordVariable: 'password')
-          ])
+            ]){
+                sh """
+               docker login -u ${usernameVariable} -p ${passwordVariable}
+               docker push  sunishsurendrank/webserver:v$BUILD_NUMBER
+                """ 
+              }
 
-          sh """
-          docker login -u ${usernameVariable} -p ${passwordVariable}
-          docker push  sunishsurendrank/webserver:v$BUILD_NUMBER
-          """
 
 }
